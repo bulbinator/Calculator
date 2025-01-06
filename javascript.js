639a70ddf6ce02db93ec24c85232cc09f056125e
bulbinator <smmaisum@gmail.com>
2025-01-06 16:22:40 -0600

    created basic math functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(i, j)
+{
+    return i + j
+}
+
+function subtract(i, j)
+{
+    return i - j
+}
+
+function multiply(i, j)
+{
+    return i * j
+}
+
+function divide(i, j)
+{
+    return i / j
+}
