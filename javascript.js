90ffd196bf8757956f1a30d91ae2c6adcd229395
bulbinator <smmaisum@gmail.com>
2025-01-06 20:42:43 -0600

    added calculator functionality

--- javascript.js
+++ javascript.js
@@ -18,26 +18,69 @@ function divide(i, j)
     return i / j
 }
 
-function operate(i, operation, j)
-{
-    return operation(i, j)
-}
-
 let calculator = document.querySelector('.calculator')
 let numbers = document.querySelector('.numbers')
+let operations = document.querySelector('.operations')
 let calcText = document.querySelector('.calcText')
 let clear = document.querySelector('.clear')
+
+let expression = []
 
 clear.addEventListener("click", function(e)
 {
     calcText.textContent = ""
+    expression = []
 })
+
 
 calculator.addEventListener("click", function(e)
 {
+
     if (e.target.tagName === "BUTTON" && numbers.contains(e.target))
     {
         console.log(e.target.textContent)
         calcText.textContent += e.target.textContent
     }
+
+    if (e.target.tagName === "BUTTON" && operations.contains(e.target))
+    {
+        expression.push(calcText.textContent)
+        expression.push(e.target.textContent)
+        calcText.textContent = ""
+    }
+
+    if (e.target.textContent === "=")
+    {
+        try
+        {
+            let answer = 0
+            let i = Number(expression.at(-4))
+            let operation = expression.at(-3)
+            let j = Number(expression.at(-2))
+
+            switch(operation)
+            {
+                case '+':
+                    answer = add(i, j)
+                    break
+                case '-':
+                    answer = subtract(i, j)
+                    break
+                case 'x':
+                    answer = multiply(i, j)
+                    break
+                case '/':
+                    answer = divide(i, j)
+                    break
+            }
+
+            calcText.textContent = answer
+            expression = []
+        }
+        catch
+        {
+            calcText.textContent = "Error"
+        }
+    }
+
 })
