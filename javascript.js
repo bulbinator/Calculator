33925815dd2abff838592ae9fc5ad317805e4bf6
bulbinator <smmaisum@gmail.com>
2025-01-06 20:02:04 -0600

    created functionality for numbers

--- javascript.js
+++ javascript.js
@@ -17,3 +17,27 @@ function divide(i, j)
 {
     return i / j
 }
+
+function operate(i, operation, j)
+{
+    return operation(i, j)
+}
+
+let calculator = document.querySelector('.calculator')
+let numbers = document.querySelector('.numbers')
+let calcText = document.querySelector('.calcText')
+let clear = document.querySelector('.clear')
+
+clear.addEventListener("click", function(e)
+{
+    calcText.textContent = ""
+})
+
+calculator.addEventListener("click", function(e)
+{
+    if (e.target.tagName === "BUTTON" && numbers.contains(e.target))
+    {
+        console.log(e.target.textContent)
+        calcText.textContent += e.target.textContent
+    }
+})
